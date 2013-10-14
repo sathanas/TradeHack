89035b4b5935429e4cf90152591f7491e4d7c1e2
AlexxHansson <alexxhansson@gmail.com>
2013-10-14 17:36:16 +0200

    Socket.io test

--- scripts/main.js
+++ scripts/main.js
@@ -7,6 +7,11 @@ require.config( {
 
 require(["jquery"], function($) 
 {
+  //Tha sockets
+  var socket = io.connect('http://194.47.142.170');
+  socket.on('newuser', function (data) {
+    console.log('Ny användare');
+  });
 
 	var url2 = "http://194.47.142.170:3000/api/products"
 	$.ajax({ url: url2} )
