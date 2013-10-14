7d954e0480c824073373e0a35f9effc49499edd5
sathanas <fredrik.gullbrandson@gmail.com>
2013-10-14 17:33:18 +0200

    init pt4

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,24 @@
-require(["helper/util"], function(util) 
-{
-    //This function is called when scripts/helper/util.js is loaded.
-    //If util.js calls define(), then this function is not fired until
-    //util's dependencies have loaded, and the util argument will hold
-    //the module value for "helper/util".
+require.config( {
+	baseUrl: "scripts/core",
+	paths: {
+		jquery: "jquery"
+	}
 });
+
+require(["jquery"], function($) 
+{
+
+	var url2 = "http://194.47.142.170:3000/api/products"
+	$.ajax({ url: url2} )
+  .done(function() {
+    console.log( "second success" );
+  })
+  .fail(function() {
+    console.log( "error" );
+  })
+  .always(function() {
+    console.log( "complete" );
+  });
+
+});
+
